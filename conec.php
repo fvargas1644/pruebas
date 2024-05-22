3f966b1623b825f3a4955b312a9d84a0abb2bb84
fvargas1644 <113227292+fvargas1644@users.noreply.github.com>
2024-05-21 20:18:51 -0500

    Create conec.php

---
+++ conec.php
@@ -0,0 +1,24 @@
+<?php
+$env = parse_ini_file("/var/local/env");
+$username = $env['username'];
+$password = $env['password'];
+$host = $env['host'];
+$base = $env['base'];
+$engine = "mysql";
+$port = "3306";
+
+// Establecer la conexión
+try {
+    $dsn = "$engine:host=$host;port=$port;dbname=$base";
+    $options = array(
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    );
+
+    $dbh = new PDO($dsn, $username, $password, $options);
+    echo "Conexión establecida exitosamente.";
+} catch (PDOException $e) {
+    echo "Error de conexión: " . $e->getMessage();
+}
+?>
+
